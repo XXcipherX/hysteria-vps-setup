8990e7c975bbcdacad77555a7b1401cd37894b12
root <root@instance64481.waicore.network>
2026-01-27 20:39:35 +0100

    Update & disable masquerade

---
+++ README.md
@@ -0,0 +1,5 @@
+```bash
+git clone https://github.com/XXcipherX/hysteria-vps-setup.git
+cd hysteria-vps-setup
+bash hy2-setup.sh
+```
--- hy2-setup.sh
+++ hy2-setup.sh
@@ -4,13 +4,11 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-# Check if script started as root
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
-# Install idn
 apt-get update
 apt-get install idn sudo -y
 
@@ -20,6 +18,22 @@ export DOMAIN=$(echo "$input_domain" | idn)
 read -ep "Enter your email: " input_email
 export EMAIL=$(echo "$input_email")
 
+read -ep "Do you want to configure server security? Do this on first run only. [y/N]: " configure_ssh_input
+if [[ ${configure_ssh_input,,} == "y" ]]; then
+
+read -ep "Enter SSH port: " input_ssh_port
+
+  read -ep "Enter SSH public key: " input_ssh_pbk
+  echo "$input_ssh_pbk" > ./test_pbk
+  ssh-keygen -l -f ./test_pbk
+  PBK_STATUS=$(echo $?)
+  if [ "$PBK_STATUS" -eq 255 ]; then
+    echo "Can't verify the public key. Try again and make sure to include 'ssh-rsa' or 'ssh-ed25519' followed by 'user@pcname' at the end of the file."
+    exit
+  fi
+  rm ./test_pbk
+fi
+
 docker_install() {
   curl -fsSL https://get.docker.com | bash
 }
@@ -27,9 +41,11 @@ docker_install() {
 if ! command -v docker >/dev/null 2>&1; then
     docker_install
 fi
-
+export SSH_USER=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 8; echo)
+export SSH_USER_PASS=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 20; echo)
 export PASS=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 20; echo)
 export SALAMANDER_PASS=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 25; echo)
+export SSH_PORT=${input_ssh_port:-22}
 
 hysteria_setup() {
   mkdir -p /opt/hysteria-vps-setup
@@ -42,10 +58,59 @@ hysteria_setup() {
 
 hysteria_setup
 
+sshd_edit() {
+  grep -r Port /etc/ssh -l | xargs -n 1 sed -i -e "/Port /c\Port $SSH_PORT"
+  grep -r PasswordAuthentication /etc/ssh -l | xargs -n 1 sed -i -e "/PasswordAuthentication /c\PasswordAuthentication no"
+  grep -r PermitRootLogin /etc/ssh -l | xargs -n 1 sed -i -e "/PermitRootLogin /c\PermitRootLogin no"
+  systemctl daemon-reload
+  systemctl restart ssh
+}
+
+add_user() {
+  useradd $SSH_USER -s /bin/bash
+  usermod -aG sudo $SSH_USER
+  echo $SSH_USER:$SSH_USER_PASS | chpasswd
+  mkdir -p /home/$SSH_USER/.ssh
+  touch /home/$SSH_USER/.ssh/authorized_keys
+  echo $input_ssh_pbk >> /home/$SSH_USER/.ssh/authorized_keys
+  chmod 700 /home/$SSH_USER/.ssh/
+  chmod 600 /home/$SSH_USER/.ssh/authorized_keys
+  chown $SSH_USER:$SSH_USER -R /home/$SSH_USER
+  usermod -aG docker $SSH_USER
+}
+
+edit_iptables() {
+  debconf-set-selections <<EOF
+iptables-persistent iptables-persistent/autosave_v4 boolean true
+iptables-persistent iptables-persistent/autosave_v6 boolean true
+EOF
+
+  apt-get -y install iptables-persistent netfilter-persistent
+  iptables -A INPUT -p icmp -j ACCEPT
+  iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+  iptables -A INPUT -p tcp -m conntrack --ctstate NEW --dport $SSH_PORT -j ACCEPT
+  iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+  iptables -A INPUT -p udp --dport 443 -j ACCEPT
+  iptables -A INPUT -i lo -j ACCEPT
+  iptables -A OUTPUT -o lo -j ACCEPT
+  iptables -P INPUT DROP
+  netfilter-persistent save
+}
+
+if [[ ${configure_ssh_input,,} == "y" ]]; then
+  sshd_edit
+  add_user
+  edit_iptables
+fi
+
 end_script() {
     docker compose -f /opt/hysteria-vps-setup/docker-compose.yml up -d
+    if [[ ${configure_ssh_input,,} == "y" ]]; then
+      echo "New user for ssh: $SSH_USER, password for user: $SSH_USER_PASS. New port for SSH: $SSH_PORT."
+    fi
     echo ""
     echo "hysteria2://$PASS@$DOMAIN:443?obfs=salamander&obfs-password=$SALAMANDER_PASS&insecure=0#"
+    echo ""
 }
 
 end_script
